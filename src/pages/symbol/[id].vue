1de48dc07ba60d08b1e7c5b76d16d3328a8f9dfd
NamPE286 <nambuihung654@gmail.com>
2025-04-12 03:29:43 +0700

    make symbol header sticky

--- src/pages/symbol/[id].vue
+++ src/pages/symbol/[id].vue
@@ -8,14 +8,16 @@ import TabPanel from 'primevue/tabpanel'
 </script>
 
 <template>
-  <SymbolHeader></SymbolHeader>
   <Tabs value="0">
-    <TabList>
-      <Tab value="0">Overview</Tab>
-      <Tab value="1">News</Tab>
-      <Tab value="2">Discussions</Tab>
-    </TabList>
-    <TabPanels>
+    <div class="sticky pt-[10px] mt-[-10px] top-0 bg-[#ffffff] dark:bg-[#121212]">
+      <SymbolHeader></SymbolHeader>
+      <TabList>
+        <Tab value="0">Overview</Tab>
+        <Tab value="1">News</Tab>
+        <Tab value="2">Discussions</Tab>
+      </TabList>
+    </div>
+    <TabPanels class="h-[1000px]">
       <TabPanel value="0"> a </TabPanel>
       <TabPanel value="1"> b </TabPanel>
       <TabPanel value="2"> c </TabPanel>
@@ -24,7 +26,12 @@ import TabPanel from 'primevue/tabpanel'
 </template>
 
 <style lang="css" scoped>
-.p-tabs * {
+.p-tabpanels {
   background: transparent;
+
+}
+
+.p-tablist * {
+  background-color: transparent;
 }
 </style>
