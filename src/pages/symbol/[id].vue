51185beea2fdfccbab3e6adc03f383eeea7d7a32
NamPE286 <nambuihung654@gmail.com>
2025-04-11 23:30:02 +0700

    minor text size change

--- src/pages/symbol/[id].vue
+++ src/pages/symbol/[id].vue
@@ -1,28 +1,7 @@
 <script setup lang="ts">
-import Select from 'primevue/select'
-
-let selectedOption = 'NASDAQ'
-const arr: string[] = ['NASDAQ', 'A', 'B']
+import SymbolHeader from '@/components/symbol/SymbolHeader.vue';
 </script>
 
 <template>
-  <div>
-    <div class="flex flex-row gap-[10px] items-center">
-      <img
-        class="h-[50px] w-[50px] rounded-full"
-        src="https://s3-symbol-logo.tradingview.com/apple--big.svg"
-        alt="company logo"
-      />
-      <div>
-        <h3 class="font-bold text-xl">Apple Inc. (AAPL)</h3>
-        <p><span class="text-3xl font-bold">200.00</span> USD <span class="font-bold text-green-500">+2.23 (+1.22%)</span></p>
-      </div>
-      <Select
-        v-model="selectedOption"
-        :options="arr"
-        placeholder="Select a market"
-        class="w-full md:w-56 ml-auto mr-[50px]"
-      ></Select>
-    </div>
-  </div>
+  <SymbolHeader></SymbolHeader>
 </template>
